ddfaba37b63502761dab79f4b6209ca506de04f9
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2015-07-15 20:34:32 +0200

    Format

--- client/app/directive/tags.js
+++ client/app/directive/tags.js
@@ -2,27 +2,27 @@ app.directive('tags', function() {
   return {
     controller : function($scope, $http) {
       $scope.loadTags = function(query) {
-      return $http.get('/api/tags?query=' + query);
-    };
+        return $http.get('/api/tags?query=' + query);
+      };
 
-    $scope.onTagAdded = function(tag) {
-      $http.post('/api/images/' + $scope.id + '/tags', tag).success(function(data) {
-        if (!tag.id) {
-          tag.id = data.id;
+      $scope.onTagAdded = function(tag) {
+        $http.post('/api/images/' + $scope.id + '/tags', tag).success(function(data) {
+          if (!tag.id) {
+            tag.id = data.id;
+          }
+        });
+      };
+
+      $scope.onTagRemoved = function(tag) {
+        if (tag.id) {
+          $http.delete('/api/images/' + $scope.id + '/tags/' + tag.id);	
         }
-      });
-    };
-
-    $scope.onTagRemoved = function(tag) {
-      if (tag.id) {
-        $http.delete('/api/images/' + $scope.id + '/tags/' + tag.id);	
-      }
-    };
-  },
-  restrict: 'E',
-  scope: {
-    image: '=image'
-  },
-  templateUrl: 'templates/directives/tags.html'
+      };
+    },
+    restrict: 'E',
+    scope: {
+      image: '=image'
+    },
+    templateUrl: 'templates/directives/tags.html'
   };
 });
