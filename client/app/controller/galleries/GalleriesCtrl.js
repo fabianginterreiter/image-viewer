95f448bb827a1ea50484412ea7f9ac25f064a593
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2015-07-15 20:49:24 +0200

    Format.

--- client/app/controller/galleries/GalleriesCtrl.js
+++ client/app/controller/galleries/GalleriesCtrl.js
@@ -1,9 +1,9 @@
-app.controller('GalleriesCtrl', ['$scope', '$http', '$routeParams', 'GalleryService', function($scope, $http, $routeParams, GalleryService) {
+app.controller('GalleriesCtrl', ['$scope', '$http', 'GalleryService', function($scope, $http, GalleryService) {
   $http.get('/api/galleries').success(function(data) {
-  	$scope.galleries = data;
+    $scope.galleries = data;
   });
 
   $scope.create = function() {
-  	GalleryService.create([]);
+    GalleryService.create([]);
   };
 }]);
