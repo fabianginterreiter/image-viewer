b745eeca5aa9e30d4e42d8ed0fa3e5cab46818fa
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2015-07-15 20:30:54 +0200

    Format file

--- client/app/directive/tags.js
+++ client/app/directive/tags.js
@@ -1,29 +1,28 @@
 app.directive('tags', function() {
   return {
-    controller : function($scope, $element, $attrs, $http, usSpinnerService, ImageCache, $timeout, $modalStack, $location) {
-     $scope.loadTags = function(query) {
-	    return $http.get('/api/tags?query=' + query);
-	  };
+    controller : function($scope, $http) {
+      $scope.loadTags = function(query) {
+      return $http.get('/api/tags?query=' + query);
+    };
 
-	  $scope.onTagAdded = function(tag) {
-	  	$http.post('/api/images/' + $scope.id + '/tags', tag).success(function(data) {
-	  		if (!tag.id) {
-	  			tag.id = data.id;
-	  		}
-	  	});
-	  };
+    $scope.onTagAdded = function(tag) {
+      $http.post('/api/images/' + $scope.id + '/tags', tag).success(function(data) {
+        if (!tag.id) {
+          tag.id = data.id;
+        }
+      });
+    };
 
-	  $scope.onTagRemoved = function(tag) {
-	  	if (tag.id) {
-	  		$http.delete('/api/images/' + $scope.id + '/tags/' + tag.id);	
-	  	}
-	  };
-    },
-
-    restrict: 'E',
-    scope: {
-      image: '=image'
-    },
-    templateUrl: 'templates/directives/tags.html'
+    $scope.onTagRemoved = function(tag) {
+      if (tag.id) {
+        $http.delete('/api/images/' + $scope.id + '/tags/' + tag.id);	
+      }
+    };
+  },
+  restrict: 'E',
+  scope: {
+    image: '=image'
+  },
+  templateUrl: 'templates/directives/tags.html'
   };
 });
