619ac11253924b3e12d973bd8f6e3db7ff945d0b
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2015-07-17 17:00:24 +0200

    Extract download service to separate file.

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -4,20 +4,3 @@ String.prototype.endsWith = function(suffix) {
 
 var app = angular.module('imageApp', ['ngRoute', 'ui.bootstrap', 'angularSpinner', 'ngTagsInput', 'autocomplete', 'ngLoad', 'angular-table']);
 
-app.factory('DownloadService', function() {
-  return {
-    images : function(images, width) {
-      var ids = [];
-
-      images.forEach(function(image) {
-        ids.push(image.id);
-      });
-
-      window.location = '/api/download/images/' + ids.join('+') + (width ? '?width=' + width : '');
-    },
-
-    gallery : function(gallery, width) {
-      window.location = '/api/download/gallery/' + gallery.id + '?name=' + gallery.name + '.zip' + (width ? '&width=' + width : '');
-    }
-  };
-});
