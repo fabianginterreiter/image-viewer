cec4a3eba723734c59f3e0a050bc4cc3b147cb12
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2015-07-21 23:36:33 +0200

    Remove not used requests.

--- client/app/controller/galleries/GalleryDetailsCtrl.js
+++ client/app/controller/galleries/GalleryDetailsCtrl.js
@@ -1,28 +1,5 @@
 app.controller('GalleryDetailsCtrl', ['$scope', '$http', '$routeParams', '$window', 'Dialogs', '$location', 'usSpinnerService', function($scope, $http, $routeParams, $window, Dialogs, $location, usSpinnerService) {
-  $scope.imagesWithGps = 0;
-  $scope.imagesWithoutGps = 0;
-
   $http.get('/api/galleries/' + $routeParams.id).success(function(data) {
   	$scope.gallery = data;
-
-    _.forEach(data.images, function(image) {
-      if (image.gps) {
-        $scope.imagesWithGps++;
-      } else {
-        $scope.imagesWithoutGps++;
-      }
-    });
-  });
-
-  $http.get('/api/galleries/' + $routeParams.id + '/directories').success(function(data) {
-  	$scope.directories = data;
-  });
-
-  $http.get('/api/galleries/' + $routeParams.id + '/tags').success(function(data) {
-  	$scope.tags = data;
-  });
-
-  $http.get('/api/galleries/' + $routeParams.id + '/persons').success(function(data) {
-  	$scope.persons = data;
   });
 }]);
