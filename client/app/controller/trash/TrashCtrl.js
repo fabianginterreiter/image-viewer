f55ff47a5ef771f7aed5172d85a7a0a8c1b583c1
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2015-08-02 19:19:20 +0200

    Add function to delete all images in trash and to restore files.

--- client/app/controller/trash/TrashCtrl.js
+++ client/app/controller/trash/TrashCtrl.js
@@ -1,5 +1,37 @@
-app.controller('TrashCtrl', ['$scope', '$http', function($scope, $http) {
+app.controller('TrashCtrl', ['$scope', '$http', 'Dialogs', function($scope, $http, Dialogs) {
   $http.get('/api/trash').success(function(data) {
     $scope.images = data;
   });
+
+  var getSelected = function() {
+    var selected = [];
+    $scope.images.forEach(function(image) {
+      if (image.selected) {
+        selected.push(image);
+      }
+    });
+    return selected;
+  };
+
+  $scope.delete = function() {
+    Dialogs.delete('Delete', 'Do you really want to delete all the files?', function() {
+      $http.get('/api/trash/delete').success(function(data) {
+
+      });
+    });
+  };
+
+  $scope.restore = function() {
+
+  	var selected = getSelected();
+
+  	var ids = [];
+    selected.forEach(function(image) {
+      ids.push(image.id);
+    });
+
+  	$http.put('/api/trash/restore/' + ids.join('+')).success(function(data) {
+
+    });
+  };
 }]);
--- routes/trash.js
+++ routes/trash.js
@@ -16,5 +16,37 @@ router.get('/', function(req, res) {
   });
 });
 
+router.get('/delete', function(req, res) {
+  database.connect(function(err, client, done) {
+    client.query('SELECT directories.path || images.name AS path FROM images JOIN directories ON images.directory_id = directories.id WHERE deleted = true', [], function(err, result) {
+
+      var loaded = _.after(result.rows.length, function() {
+        client.query('DELETE FROM images WHERE deleted = true', [], function(err, client) {
+          res.send("OK");
+        });
+      });
+
+      _.forEach(result.rows, function(image) {
+        var path = req.config.get('root') + image.path;
+
+        console.time().tag('Trash').info('Deleting: ' + path);
+        fs.unlink(path, function(err) {
+          if (err) {
+
+          }
+          loaded();
+        })
+      });
+    });
+  });
+});
+
+router.get('/restore/:ids', function(req, res) {
+  var ids = req.param('ids').split('+');
+  database.query('UPDATE images SET deleted = false WHERE deleted = true AND id IN(' + ids.join(',') + ')', [], function(err, result) {
+    res.send('OK');
+  });
+});
+
 
 module.exports = router;
