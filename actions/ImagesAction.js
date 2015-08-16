b04aacf2c36e76a05219364420b7f86e29da709c
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2015-08-16 16:32:30 +0200

    Create search images controller.

--- actions/ImagesAction.js
+++ actions/ImagesAction.js
@@ -10,6 +10,7 @@ function updateImage(client, id, callback) {
 }
 
 import { GetImageController } from '../controllers/images/GetImageController';
+import { SearchImagesController } from '../controllers/images/SearchImagesController';
 
 export class ImagesAction {
   constructor(database) {
@@ -56,70 +57,10 @@ export class ImagesAction {
         });
         break;
       case 'search':
-        var conditions = [];
-        conditions.push('1 = 1');
-
-        if (options.persons) {
-          var personIds = options.persons.split(',');
-          _.forEach(personIds, function(id) {
-            conditions.push('EXISTS (SELECT 1 FROM image_person WHERE images.id = image_person.image_id AND image_person.person_id = ' + id + ')');
-          });
-
-          if (options.personsOnly === 'true') {
-            conditions.push('EXISTS (SELECT 1 FROM image_person WHERE image_person.image_id = images.id GROUP BY image_person.image_id HAVING count(image_person.image_id) = ' + personIds.length + ')');
-          }
-        } else if (options.personsOnly === 'true') {
-          conditions.push('NOT EXISTS (SELECT 1 FROM image_person WHERE image_person.image_id = images.id)');
-        }
-
-        console.tag('Search').time().info('Search!!!');
-
-        if (options.tags) {
-          var tagIds = options.tags.split(',');
-          _.forEach(tagIds, function(id) {
-            conditions.push('EXISTS (SELECT 1 FROM image_tag WHERE images.id = image_tag.image_id AND image_tag.tag_id = ' + id + ')');
-          });
-
-          if (options.tagsOnly === 'true') {
-            conditions.push('EXISTS (SELECT 1 FROM image_tag WHERE image_tag.image_id = images.id GROUP BY image_tag.image_id HAVING count(image_tag.image_id) = ' + tagIds.length + ')');
-          }
-        } else if (options.tagsOnly === 'true') {
-          conditions.push('NOT EXISTS (SELECT 1 FROM image_tag WHERE image_tag.image_id = images.id)');
-        }
-
-        if (options.galleries) {
-          var galleryIds = options.galleries.split(',');
-          _.forEach(galleryIds, function(id) {
-            conditions.push('EXISTS (SELECT 1 FROM gallery_image WHERE images.id = gallery_image.image_id AND gallery_image.gallery_id = ' + id + ')');
-          });
-
-          if (options.galleriesOnly === 'true') {
-            conditions.push('EXISTS (SELECT 1 FROM gallery_image WHERE gallery_image.image_id = images.id GROUP BY gallery_image.image_id HAVING count(gallery_image.image_id) = ' + galleryIds.length + ')');
-          }
-        } else if (options.galleriesOnly === 'true') {
-          conditions.push('NOT EXISTS (SELECT 1 FROM gallery_image WHERE gallery_image.image_id = images.id)');
-        }
-
-        if (options.minDate) {
-          conditions.push('\''+options.minDate+'\' <= images.created_at');
-        }
-
-        if (options.maxDate) {
-          conditions.push('\''+options.maxDate+'\' >= images.created_at');
-        }
-
-        var query = 'SELECT images.* FROM images WHERE ' + conditions.join(' AND ') + ' ORDER BY images.created_at LIMIT 200';
-
-        console.time().tag('Search').info(query);
-
         this.database.connect(function(err, client, done) {
-          client.query(query, [], function(err, result) {
-            if (err) {
-              done();
-              callback(err);
-            }
+          new SearchImagesController(client).search(options, function (err, result) {
             done();
-            callback(null, result.rows);
+            callback(null, result);
           });
         });
         break;
