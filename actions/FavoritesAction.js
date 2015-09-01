7d91c49d7261995588b246b7c5c5a1f1af666075
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2015-09-01 23:36:18 +0200

    Use knew for other web services.

--- actions/FavoritesAction.js
+++ actions/FavoritesAction.js
@@ -18,28 +18,34 @@ export class FavoritesAction {
   }
 
   get(user, callback) {
-    knex.select('images.*').from('images').join('user_image', 'images.id', 'user_image.image_id').where({
+    knex('images').select('images.*').join('user_image', 'images.id', 'user_image.image_id').where({
       'user_image.user_id' : user.id
     }).then(function(rows) {
       callback(null, rows);
+    }).catch(function(error) {
+      callback(error);
     });
   }
 
   add(user, imageId, callback) {
-    this.database.connect(function(err, client, done) {
-      client.query('INSERT INTO user_image (user_id, image_id) VALUES($1, $2) RETURNING image_id', [user.id, imageId], function(err, result) {
-        if (err) {
-          return callback(err);
-        }
-        done();
-        callback(null, 'OK');
-      });
+    knex('user_image').insert({
+      'user_id' : user.id,
+      'image_id' : imageId
+    }).then(function(rows) {
+      callback(null, 'OK');
+    }).catch(function(error) {
+      callback(error);
     });
   }
 
   delete(user, imageId, callback) {
-    this.database.query('DELETE FROM user_image WHERE user_id = $1 AND image_id = $2', [user.id, imageId], function(err, result) {
-      callback(null, result);
+    knex('user_image').where({
+      'user_id' : user.id,
+      'image_id' : imageId
+    }).del().then(function() {
+      callback(null, 'OK');
+    }).catch(function(error) {
+      callback(error);
     });
   }
 
