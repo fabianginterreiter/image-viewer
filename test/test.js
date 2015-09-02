07a3517066ab314defa190cbe72d42085b5a7a06
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2015-09-02 21:18:20 +0200

    Add first memory db test.

--- test/test.js
+++ test/test.js
@@ -4,6 +4,33 @@ import { TagController } from '../controllers/tag';
 
 describe('Tags', function() {
   describe('Getting a Tag', function() {
+    var knex = require('knex')({
+      client: 'sqlite3',
+      debug: true,
+      connection: {
+        filename: ':memory:'
+      }
+    });
+
+    knex.schema.createTable('users', function (table) {
+      table.increments();
+      table.string('name');
+      table.timestamps();
+    }).createTable('accounts', function(table) {
+      table.increments('id');
+      table.string('account_name');
+      table.integer('user_id').unsigned().references('users.id');
+    }).then(function() {
+      return knex.insert({
+        'name' : 'Fabian'
+      }).into('users');
+    }).then(function() {
+      console.log("select");
+      return knex('users').select('name').then(function(rows) {
+        console.log(rows);
+      });
+    });
+
     it('Should return error in case of Database error.', () => {
       new TagController({
         connect : function(callback) {
@@ -11,20 +38,6 @@ describe('Tags', function() {
         }
       }).get(1, function(err, call) {
         equal(err, "error");
-      });
-    });
-
-    it('Should return a list of tags and images', () => {
-      new TagController({
-        connect : function(callback) {
-          callback(null, function() {
-            
-          }, function() {
-
-          });
-        }
-      }).get(1, function(err, call) {
-        equal(err, null);
       });
     });
   });
