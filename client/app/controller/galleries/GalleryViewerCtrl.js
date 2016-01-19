58e01cba3af34eafb1cf15258c92f2e2c8b2fc0e
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2016-01-19 21:55:12 +0100

    Stop spinner after loading data.

--- client/app/controller/galleries/GalleryViewerCtrl.js
+++ client/app/controller/galleries/GalleryViewerCtrl.js
@@ -1,9 +1,15 @@
 app.controller('GalleryViewerCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
   $http.get('/api/galleries/' + $routeParams.gallery_id).success(function(data) {
   	$scope.gallery = data;
+    if ($scope.image) {
+      usSpinnerService.stop('browse');
+    }
   });
 
   $http.get('/api/images/' + $routeParams.id).success(function(data) {
   	$scope.image = data;
+    if ($scope.gallery) {
+      usSpinnerService.stop('browse');
+    }
   });
 }]);
