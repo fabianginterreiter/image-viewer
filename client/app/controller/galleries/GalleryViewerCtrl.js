5986fdb0d97bc66e08e5dcfd9fae16a28c9555c1
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2016-01-19 21:56:56 +0100

    Format code add dependency for spinner.

--- client/app/controller/galleries/GalleryViewerCtrl.js
+++ client/app/controller/galleries/GalleryViewerCtrl.js
@@ -1,13 +1,13 @@
-app.controller('GalleryViewerCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+app.controller('GalleryViewerCtrl', ['$scope', '$http', '$routeParams', 'usSpinnerService', function($scope, $http, $routeParams, usSpinnerService) {
   $http.get('/api/galleries/' + $routeParams.gallery_id).success(function(data) {
-  	$scope.gallery = data;
+    $scope.gallery = data;
     if ($scope.image) {
       usSpinnerService.stop('browse');
     }
   });
 
   $http.get('/api/images/' + $routeParams.id).success(function(data) {
-  	$scope.image = data;
+    $scope.image = data;
     if ($scope.gallery) {
       usSpinnerService.stop('browse');
     }
